f3bbdfbfad819df63ece9095bf3191e305a32c62
Darrin Gambino <darrin@bothminds.com>
2025-07-23 16:34:10 -0400

    initial

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+    <TheWelcome />
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+
+}
+</style>
